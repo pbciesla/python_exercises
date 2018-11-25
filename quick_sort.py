776ee97d7cfd37bd39ef718301cd9e30a2e8703f
Patrycja Cieśla <pbciesla@gmail.com>
2018-11-25 09:00:23 +0100

    Little correct

--- quick_sort.py
+++ quick_sort.py
@@ -7,11 +7,11 @@ def quick_sort(to_sort):
     else:
         mid = to_sort[0]
         for i in to_sort[1:]:
-            if i < mid:
+            if i <= mid:
                 left.append(i)
             elif i > mid:
                 right.append(i)
     return quick_sort(left) + [mid] + quick_sort(right)
 
 
-print(quick_sort([2, 6, 12, 4, 78, 52, 10, 3, 65, 9, 45, 0]))
+print(quick_sort([6, 1, 7, 4, 3, 9, 6, 3, 2, 6, 5]))
